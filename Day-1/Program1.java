29e583b8459a6d495bc2aba742fbb9e9729e28eb
princebhati130 <princegujjar1965@gmail.com>
2026-06-06 21:27:48 +0530

    Update Program1.java

--- Day-1/Program1.java
+++ Day-1/Program1.java
@@ -1,1 +1,24 @@
-.
+import java.util.Scanner;
+
+public class Program1 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        // Sum
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        // Output
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
