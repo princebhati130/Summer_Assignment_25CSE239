5f25e4a67a5847fe5f97379de8fd4b2019549d65
princebhati130 <princegujjar1965@gmail.com>
2026-06-09 23:30:35 +0530

    Create program23.java

---
+++ Day-6/program23.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Program23 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        // Count
+        int count = 0;
+
+        while (n > 0) {
+
+            if (n % 2 == 1) {
+                count++;
+            }
+
+            n = n / 2;
+        }
+
+        // Output
+        System.out.println("Set Bits = " + count);
+
+        sc.close();
+    }
+}
