535394f4bfdd5b6d46fbc3d965d3859b50e89f25
princebhati130 <princegujjar1965@gmail.com>
2026-06-09 23:31:10 +0530

    Create program24.java

---
+++ Day-6/program24.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program24 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Base (x): ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter Power (n): ");
+        int n = sc.nextInt();
+
+        // Power
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= x;
+        }
+
+        // Output
+        System.out.println("Result = " + result);
+
+        sc.close();
+    }
+}
