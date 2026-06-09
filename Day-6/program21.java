659db505d8d50eba33b3ffb35ce3590e60d0dea4
princebhati130 <princegujjar1965@gmail.com>
2026-06-09 23:29:11 +0530

    Create program21.java

---
+++ Day-6/program21.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Program21 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Decimal Number: ");
+        int n = sc.nextInt();
+
+        // Binary
+        String binary = "";
+
+        while (n > 0) {
+            binary = (n % 2) + binary;
+            n = n / 2;
+        }
+
+        // Output
+        System.out.println("Binary = " + binary);
+
+        sc.close();
+    }
+}
