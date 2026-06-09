312416fc0bb5291c531fc9d63873db81002d45f5
princebhati130 <princegujjar1965@gmail.com>
2026-06-09 23:30:04 +0530

    Create program22.java

---
+++ Day-6/program22.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Program22 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Binary Number: ");
+        int binary = sc.nextInt();
+
+        // Variables
+        int decimal = 0;
+        int power = 0;
+
+        // Conversion
+        while (binary != 0) {
+
+            int digit = binary % 10;
+
+            decimal += digit * (int)Math.pow(2, power);
+
+            power++;
+            binary /= 10;
+        }
+
+        // Output
+        System.out.println("Decimal = " + decimal);
+
+        sc.close();
+    }
+}
