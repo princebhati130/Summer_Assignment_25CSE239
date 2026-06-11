8cedb690751a6c256cb2ab52eeb8cb28c74eb99a
princebhati130 <princegujjar1965@gmail.com>
2026-06-11 14:40:57 +0530

    Create Program27.java

---
+++ Day-7/Program27.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Program27 {
+
+    static int sumDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum of Digits = " + sumDigits(n));
+
+        sc.close();
+    }
+}
