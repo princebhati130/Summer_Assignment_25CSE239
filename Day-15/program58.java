f20992830b10d04bf95430b1ba5f8971db7f3f20
princebhati130 <princegujjar1965@gmail.com>
2026-06-22 22:07:14 +0530

    Create program58.java

---
+++ Day-15/program58.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Program58 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[n - 1] = first;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
