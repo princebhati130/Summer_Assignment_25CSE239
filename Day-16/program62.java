179dee118ac6350c1d3156d4ce4512132103ce11
princebhati130 <princegujjar1965@gmail.com>
2026-06-22 22:10:20 +0530

    Create program62.java

---
+++ Day-16/program62.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Program62 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int maxFreq = 0;
+        int element = arr[0];
+
+        for (int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count > maxFreq) {
+                maxFreq = count;
+                element = arr[i];
+            }
+        }
+
+        System.out.println("Element = " + element);
+
+        sc.close();
+    }
+}
